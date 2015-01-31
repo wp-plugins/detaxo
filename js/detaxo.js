59f7dc50c0ed0e254bc1fad8a6c5ba800d4d324c
wp_harish <wp_harish@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-31 07:20:52 +0000

    Added Detaxo-1.0.0 version.
    git-svn-id: https://plugins.svn.wordpress.org/detaxo/trunk@1079457 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/detaxo.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+jQuery(document).ready(function() {
+    jQuery(".custom_link_detach_category").live("click", function() {
+        var id = jQuery(this).attr("id");
+        var arrVal = id.split("_");
+        var cat_id = arrVal[1];
+        var post_ids = arrVal[0];
+        jQuery.ajax({
+            "url": ajaxurl,
+            "type": "post",
+            dataType: 'json',
+            "data": {
+                "post": post_ids,
+                "cat": cat_id,
+                "action": "dtxo_detach_category"
+            },
+            "success": function(resp) {
+                if (resp.ack == '1') {
+                    jQuery("#post-" + post_ids).remove();
+                    if (!jQuery("#the-list tr").length) {
+                        jQuery("#the-list").html('<tr class="no-items"><td colspan="8"class="colspanchange">No posts found.</td></tr>');
+                    }
+                }
+            }
+        });
+        return false;
+    });
+});
---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== DeTaxo ===
+Contributors: wp_harish 
+Donate link: 
+Tags: Wordpress, Harish Moghe, Detaxo, Taxonomy, post lists, post management, taxonomy management, category management, post category management,custom posts, custom post management, custom taxonomy
+Requires at least: 3.0.1
+Tested up to: 3.9
+Stable tag: 3.9
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+While using category filter on posts list in wordpress backend . Some where you found that the post is listed in wrong category.
+DeTaxo provides column to have link to de-attach the post from the category, No need to quick edit or edit post. deTaxo removes category from post on the fly with just one click.
+
+== Installation ==
+
+1. Download and extraxt deTaxo.zip and put it in '/wp-content/plugins/` directory
+2. Go to plugin lists activate plugin. 
+3. Go to settings -> DeTaxo settings to manage post types for which deTaxo should apply.
+
+== Screenshots ==
+
+1. /assets/screenshot-1.jpg
